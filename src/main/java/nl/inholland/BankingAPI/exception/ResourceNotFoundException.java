67756584052e3d3337dde8b25328390deee95f33
TimRof <70700788+TimRof@users.noreply.github.com>
2022-04-21 23:13:11 +0200

    Add files via upload

---
+++ src/main/java/nl/inholland/BankingAPI/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package nl.inholland.BankingAPI.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+    public ResourceNotFoundException(String msg) {
+        super(msg);
+    }
+}
