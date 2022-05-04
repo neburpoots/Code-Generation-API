48d605b216de586d6cce85852632a4081f0d0346
TimRof <70700788+TimRof@users.noreply.github.com>
2022-05-04 21:48:22 +0200

    Delete src directory

--- src/main/java/nl/inholland/BankingAPI/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package nl.inholland.BankingAPI.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-    public ResourceNotFoundException(String msg) {
-        super(msg);
-    }
-}
