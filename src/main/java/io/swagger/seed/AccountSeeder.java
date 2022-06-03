83ce3a7d1d8df4135ad3ac35dba4c08c486fe4c3
TimRof <70700788+TimRof@users.noreply.github.com>
2022-06-03 21:36:55 +0200

    Bank now doesn't appear in User search

--- src/main/java/io/swagger/seed/AccountSeeder.java
+++ src/main/java/io/swagger/seed/AccountSeeder.java
@@ -41,20 +41,16 @@ public class AccountSeeder {
         Account account4 = new Account(new BigDecimal(5000), new BigDecimal(-100), AccountType.SAVINGS, false);
         Account account5 = new Account(new BigDecimal(3000), new BigDecimal(-200), AccountType.PRIMARY, true);
         Account account6 = new Account(new BigDecimal(4000), new BigDecimal(-100), AccountType.SAVINGS, true);
-        Account account7 = new Account(new BigDecimal(2000), new BigDecimal(-100), AccountType.PRIMARY, true);
-        Account account8 = new Account(new BigDecimal(2000), new BigDecimal(-150), AccountType.SAVINGS, true);
 
-        account1.setUser(users.get(0));
-        account2.setUser(users.get(0));
-        account3.setUser(users.get(1));
-        account4.setUser(users.get(1));
-        account5.setUser(users.get(2));
-        account6.setUser(users.get(2));
-        account7.setUser(users.get(3));
-        account8.setUser(users.get(3));
+        account1.setUser(users.get(1));
+        account2.setUser(users.get(1));
+        account3.setUser(users.get(2));
+        account4.setUser(users.get(2));
+        account5.setUser(users.get(3));
+        account6.setUser(users.get(3));
 
         return this.accountRepo.saveAll(
-                List.of(bankaccount, account1, account2, account3, account4, account5, account6, account7, account8)
+                List.of(bankaccount, account1, account2, account3, account4, account5, account6)
         );
     }
 }
